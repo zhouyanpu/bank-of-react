9e45336a2263f7b05e5df06688cc47cf632433e0
zhouyanpu <zhouyanpu@gmail.com>
2019-07-29 15:44:42 -0400

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import Credit_page from "./components/credit";
+import Debit_page from "./components/debit";
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className="App">Hello</div>
+        <div className="App">
+          Hello
+          <Credit_page />
+          <Debit_page />
+        </div>
       </Router>
     );
   }
