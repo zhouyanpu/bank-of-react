ff8f601ce7c037208397d7830b8552cc79dc4ecb
mehrabha <mehrabhfz@gmail.com>
2019-07-29 22:18:43 -0400

    no message

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,9 @@ class Home extends Component{
             <div>
                 <img src= "https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt= "bank"/>
                 <h1>Bank of React</h1>
+                <p> Debit Total: {this.props.debits}</p>
+                <p> Credits Total: {this.props.credits}</p>
+                <AccountBalance accountBalance={this.props.accountBalance}/>
             </div>
         )
     }
